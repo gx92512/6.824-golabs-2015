ffddea9fcd5820eb0b9437d10b810829d98993f0
gaoxiang <gaxi2008@foxmail.com>
2016-06-02 19:27:36 +0800

    some update for lab4 part A

--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -8,6 +8,7 @@ import "log"
 import "paxos"
 import "sync"
 import "sync/atomic"
+import "time"
 import "os"
 import "syscall"
 import "encoding/gob"
@@ -27,29 +28,245 @@ type ShardMaster struct {
 
 type Op struct {
 	// Your data here.
+    Op string
+    Gid int64
+    Num int
+    Servers []string
+    Shard int
+    Me int
 }
 
+func (sm *ShardMaster) waitPaxos(seq int) Op{
+    to := 10 * time.Millisecond
+    for {
+        status, v := sm.px.Status(seq)
+        if status == paxos.Decided {
+            value := v.(Op)
+            return value
+        }
+        time.Sleep(to)
+        if to < 10 * time.Second{
+            to *= 2
+        }
+    }
+}
+
+func (sm *ShardMaster) Dojoin(value Op) {
+    length := len(sm.configs)
+    lconf := sm.configs[length - 1]
+    nconf := Config{Num: length, Shards: lconf.Shards}
+    nconf.Groups = make(map[int64][]string)
+    for k,v := range lconf.Groups {
+        nconf.Groups[k] = v
+    }
+    nconf.Groups[value.Gid] = value.Servers
+    gnum := len(nconf.Groups)
+    snum := len(lconf.Shards)
+    if gnum > snum{
+        gnum = snum
+    }
+    avg := snum / gnum
+    yu := snum % gnum
+    //fmt.Printf("Dojoin %d  %d  %d\n", value.Gid, gnum, snum)
+    var dict map[int64]int
+    dict = make(map[int64]int)
+    for i,v := range nconf.Shards{
+        _,ok := dict[v]
+        if !ok {
+            dict[v] = 0
+        }
+        if dict[v] == avg && yu > 0{
+            dict[v] += 1
+            yu -= 1
+        }else if dict[v] > avg || (dict[v] == avg && yu == 0){
+            nconf.Shards[i] = value.Gid
+        }else{
+            dict[v] += 1
+        }
+    }
+    for i,v := range nconf.Shards{
+        if v == 0{
+            nconf.Shards[i] = value.Gid
+        }
+    }
+    sm.configs = append(sm.configs, nconf)
+
+    /*
+    for k,v := range lconf.Shards{
+        fmt.Printf("%d: %d, ", k, v)
+    }
+    fmt.Printf("\n")
+    for k,v := range nconf.Shards{
+        fmt.Printf("%d: %d, ", k, v)
+    }
+    fmt.Printf("\n")
+    counts := map[int64]int{}
+    for _, g := range nconf.Shards {
+        counts[g] += 1
+    }
+    for k,v := range counts{
+        fmt.Printf("%d: %d\n", k, v)
+    }
+    */
+
+}
+
+func (sm *ShardMaster) Doleave(value Op) {
+    //fmt.Printf("Doleave: %d\n", value.Gid)
+    length := len(sm.configs)
+    lconf := sm.configs[length - 1]
+    nconf := Config{Num: length, Shards: lconf.Shards}
+    nconf.Groups = make(map[int64][]string)
+    for k,v := range lconf.Groups {
+        if k != value.Gid{
+            nconf.Groups[k] = v
+        }
+    }
+    var dict map[int64]int
+    dict = make(map[int64]int)
+    for k := range nconf.Groups{
+        _,ok := dict[k]
+        if !ok {
+            dict[k] = 0
+        }
+    }
+    for _,v := range nconf.Shards{
+        if v == value.Gid{
+            continue
+        }
+        dict[v] += 1
+    }
+    for i,v := range nconf.Shards{
+        minnum := 100
+        mink := int64(0)
+        if v == value.Gid{
+            for k,va := range dict{
+                if va < minnum{
+                    minnum = va
+                    mink = k
+                }
+            }
+            nconf.Shards[i] = mink
+            dict[mink] += 1
+        }
+    }
+    sm.configs = append(sm.configs, nconf)
+
+    /*
+    for k,v := range lconf.Shards{
+        fmt.Printf("%d: %d, ", k, v)
+    }
+    fmt.Printf("\n")
+    for k,v := range nconf.Shards{
+        fmt.Printf("%d: %d, ", k, v)
+    }
+    fmt.Printf("\n")
+    counts := map[int64]int{}
+    for _, g := range nconf.Shards {
+        counts[g] += 1
+    }
+    for k,v := range counts{
+        fmt.Printf("%d: %d\n", k, v)
+    }
+    */
+}
+
+func (sm *ShardMaster) Domove(value Op) {
+    length := len(sm.configs)
+    lconf := sm.configs[length - 1]
+    nconf := Config{Num: length, Shards: lconf.Shards}
+    nconf.Groups = make(map[int64][]string)
+    for k,v := range lconf.Groups {
+        nconf.Groups[k] = v
+    }
+    nconf.Shards[value.Shard] = value.Gid
+    sm.configs = append(sm.configs, nconf)
+}
+
+func (sm *ShardMaster) Doquery(value Op) {
+    length := len(sm.configs)
+    lconf := sm.configs[length - 1]
+    nconf := Config{Num: length, Shards: lconf.Shards}
+    nconf.Groups = make(map[int64][]string)
+    for k,v := range lconf.Groups {
+        nconf.Groups[k] = v
+    }
+    sm.configs = append(sm.configs, nconf)
+}
+
+func (sm *ShardMaster) Insert(value Op) {
+    switch value.Op {
+    case "Join":
+        sm.Dojoin(value)
+    case "Leave":
+        sm.Doleave(value)
+    case "Move":
+        sm.Domove(value)
+    case "Query":
+        sm.Doquery(value)
+    }
+}
+
+func (sm *ShardMaster) Process(logete Op) {
+    finish := false
+    var value Op
+    for !finish {
+        seq := len(sm.configs)
+        //fmt.Printf("in process %d\n", seq)
+        status, val := sm.px.Status(seq)
+        if status != paxos.Decided{
+            sm.px.Start(seq, logete)
+            value = sm.waitPaxos(seq)
+        }else {
+            value = val.(Op)
+        }
+        finish = value.Me == sm.me
+        sm.Insert(value)
+        sm.px.Done(seq)
+    }
+}
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) error {
 	// Your code here.
+    sm.mu.Lock()
+    logete := Op{Op: "Join", Gid: args.GID, Servers: args.Servers, Me: sm.me}
+    sm.Process(logete)
+    sm.mu.Unlock()
 
 	return nil
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) error {
 	// Your code here.
+    sm.mu.Lock()
+    logete := Op{Op: "Leave", Gid: args.GID, Me: sm.me}
+    sm.Process(logete)
+    sm.mu.Unlock()
 
 	return nil
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) error {
 	// Your code here.
+    sm.mu.Lock()
+    logete := Op{Op: "Move", Gid: args.GID, Shard: args.Shard, Me: sm.me}
+    sm.Process(logete)
+    sm.mu.Unlock()
 
 	return nil
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) error {
 	// Your code here.
+    sm.mu.Lock()
+    logete := Op{Op: "Query", Num: args.Num, Me: sm.me}
+    sm.Process(logete)
+    index := args.Num
+    if index == -1 || index >= len(sm.configs){
+        index = len(sm.configs) - 1
+    }
+    reply.Config = sm.configs[index]
+    sm.mu.Unlock()
 
 	return nil
 }
